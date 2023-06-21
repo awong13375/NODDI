d24ad871f606eaae813a19c1b0ab335221ef7ad4
awong13375 <alexw13375@gmail.com>
2023-06-21 14:21:49 -0400

    top up implementation

--- NODDI_processing.m
+++ NODDI_processing.m
@@ -3,8 +3,41 @@ addpath(genpath('/usr/local/NODDI_tool'))
 
 %% set FSL environment
 setenv('PATH', [getenv('PATH') ':/Users/alexw/fsl/bin']);
+addpath(genpath('/Users/alexw/fsl/bin/fslroi.sh'))
 
-%% open dataset directory
+
+%% go to dataset directory
 cd('/Users/alexw/Downloads/NODDI Project/wetransfer_noddi_2023-06-02_0338/SAH_NODDI/DICOM/')
 
 %% Pre-processing
+input='DICOM_AX_DTI_NODDI_1_20230518105839_701';
+output='DTI_NODDI_1_b0';
+fslroi=['fslroi' ' ' input ' ' output ' ' '0 ' '1'];
+system(fslroi)
+
+calibration='DICOM_AX_DTI_Calibration_20230518105839_1101';
+fslmerge=['fslmerge -t b0' ' ' output ' ' calibration ];
+system(fslmerge)
+
+%% Generate acqparams.txt
+filename='DICOM_AX_DTI_NODDI_1_20230518105839_701.json';
+metadata=jsondecode(fileread(filename));
+
+echotime=getfield(metadata, 'EchoTime');
+grappa=getfield(metadata, 'ParallelReductionFactorInPlane');
+phase_encode_steps=getfield(metadata,"PhaseEncodingSteps");
+
+echo_spacing=(echotime/grappa) / 1000;
+total_readout_time=echo_spacing * phase_encode_steps;
+
+acqparams = fopen('acqparams.txt', 'wt');
+txt=['0 ' '-1 ' '0 ' num2str(total_readout_time) ' ' '\n' '0 ' '1 ' '0 ' num2str(total_readout_time)];
+fprintf(acqparams, txt)
+fclose(acqparams)
+
+%% top up
+topup = ['topup ' '--imain=b0.nii --datain=acqparams.txt --out=my_output --fout=my_field --iout=my_unwarped_images']
+system(topup)
+
+
+
