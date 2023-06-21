807183b832dbae1d750d2be2dc93d0ecc5499159
awong13375 <alexw13375@gmail.com>
2023-06-20 22:22:53 -0400

    linking fsl to matlab

--- NODDI_processing.m
+++ NODDI_processing.m
@@ -1,2 +1,10 @@
-%include NODDI toolbox in directory
+%% include NODDI toolbox in directory
 addpath(genpath('/usr/local/NODDI_tool'))
+
+%% set FSL environment
+setenv('PATH', [getenv('PATH') ':/Users/alexw/fsl/bin']);
+
+%% open dataset directory
+cd('/Users/alexw/Downloads/NODDI Project/wetransfer_noddi_2023-06-02_0338/SAH_NODDI/DICOM/')
+
+%% Pre-processing
